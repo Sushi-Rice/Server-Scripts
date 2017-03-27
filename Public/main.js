5398d175b17c2fb680ba86574d2bd0c77d49f730
basketballhero <rondodude2000@gmail.com>
2017-03-26 22:33:51 -0700

    Update main.js

--- Public/main.js
+++ Public/main.js
@@ -1,6 +1,11 @@
 $(function () {
   var socket = io();
-  
+  $(document).keypress(function(e){
+    if(e.which === 37){
+      socket.emit('left');
+      $('#main').css("color", "blue");
+    }
+  });
   $('#button').click(function(){
     socket.emit('submission');
     $('#main').css("color", "red");
