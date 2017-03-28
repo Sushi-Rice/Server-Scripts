d027f6a45933b3359ce468cf142d4a5095fc93ac
basketballhero <rondodude2000@gmail.com>
2017-03-27 22:10:43 -0700

    Added an event that gets emitted when left gets pressed

--- Public/main.js
+++ Public/main.js
@@ -3,6 +3,8 @@ $(function () {
   $(document).keydown(function(e){
     if (e.keyCode == 37) { 
        //Stuff to be done when "left" is pressed
+       //Not sure if any data needs to be passed in the emit
+       socket.emit("left");
        return false;
     }
 });
