a724d7e20fc856c8ee32ceccfca17d1de96c2871
basketballhero <rondodude2000@gmail.com>
2017-03-28 23:06:35 -0700

    Cleaned up the code

--- Public/main.js
+++ Public/main.js
@@ -5,22 +5,14 @@ $(function () {
   var socket = io();
   $(document).keydown(function(e){
     if (e.keyCode == 37) { 
-       //Stuff to be done when "left" is pressed
-       socket.emit("left");
+       socket.emit("clicked");   
        return false;
     }
 });
+  
   $('#button').click(function(){
-    socket.emit('submission');
-    $('#main').css("color", "red");
-    socket.emit("left");
     socket.emit("clicked");
-    return false;
-    
-  });
-  socket.on('changePos', function(position){
-    //clientPosition = position;
-    $('#main').text(position);
+    return false; 
   });
   
   socket.on('posChanged', function(myPlace, myCount, totLength){
