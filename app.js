421b5b4da74c15e0f80fdd033308d81a0e2eacab
basketballhero <rondodude2000@gmail.com>
2017-03-28 22:23:54 -0700

    Added counters.length to 'posChanged'

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ io.on('connection', function (socket) {
   
   socket.on('clicked', function(){
      counters[myNumber] += 1;
-     io.emit('posChanged', myNumber, counters[myNumber]);
+     io.emit('posChanged', myNumber, counters[myNumber], counters.length);
   });
 	
   socket.emit('counterSending', counters);
